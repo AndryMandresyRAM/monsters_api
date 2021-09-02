1068b28328f863cd98d6db2774b74a833cb81bc3
Andry Mandresy RAMAHALEOHANITRARIVO <mandresy.ramahaleohanitrarivo@comdatagroup.com>
2021-09-02 23:41:37 +0200

    Adding the cors middleware

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use('/', routes)
+
+app.use(cors({
+    origin: ["http://localhost:8081"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 app.use((err, req, res, next)=> {
     res.json(err);
